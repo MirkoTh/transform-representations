fcbe7e3494ddb1162e3ad0a3ffe22fedcb443e25
MirkoTh <mirkothalmann@hotmail.com>
2021-11-09 22:35:04 +0100

    add some temporary notes

--- py_utils/utils.py
+++ py_utils/utils.py
@@ -18,3 +18,30 @@ def simulation_conditions():
         [s_conditions, s_prior_sd, s_sampling, s_constrain_space]
     )
     return df_info
+
+
+from functools import reduce
+import sys
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+
+sys.path.append("..")
+
+import py_utils.utils as utils
+
+%load_ext autoreload
+%autoreload 2
+
+*TODOs*
+- make_stimuli function
+- model function returning deviation from actual y value
+- prior function taking stimuli to be shown in the beginning and returning initially fit model
+- boundary function
+- simulation loop
+- plotting functions
