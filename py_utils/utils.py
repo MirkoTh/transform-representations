18079387df1540a34a90c36406aacc0913d26967
Mirko Thalmann <mirkothalmann@hotmail.com>
2021-11-12 11:41:04 +0100

    change list of pandas iterrow objects to list of dictionaries

--- py_utils/utils.py
+++ py_utils/utils.py
@@ -9,7 +9,7 @@ from sklearn.gaussian_process.kernels import RBF
 from statsmodels.distributions import ECDF
 
 
-def simulation_conditions(dict_variables: dict):
+def simulation_conditions(dict_variables: dict) -> tuple:
     """create dataframe and list with simulation conditions
 
     Args:
@@ -17,7 +17,7 @@ def simulation_conditions(dict_variables: dict):
 
     Returns:
          [df_info] pd.DataFrame: a data frame with the conditions
-         [l_info] list: list with conditions
+         [l_info] list: list with conditions as dicts
     """
     l = list()
     for k, v in dict_variables.items():
@@ -26,37 +26,36 @@ def simulation_conditions(dict_variables: dict):
         )
     df_info = reduce(lambda x, y: pd.merge(x, y, how="cross"), l)
     l_info = list()
-    for df_r in df_info.itertuples():
-        l_info.append(df_r)
-    return df_info, l_info
+    for i in range(0, df_info.shape[0]):
+        l_info.append(df_info.loc[i,].to_dict())
+    return (df_info, l_info)
 
 
-def make_stimuli(t_info: pd.core.frame) -> pd.DataFrame:
+def make_stimuli(dict_info: pd.core.frame) -> pd.DataFrame:
     """create x and y values of stimuli shown during training
 
     Args:
-        t_info (pd.core.frame): named vector with variables of simulation study
+        dict_info (dict): dict with variables of simulation study
 
     Returns:
         pd.DataFrame: x (1, 2, ...) and y values, each in a column
     """
 
-    # access value from named tuple for example like t_info.prior_sd
     l_x = list(
         map(
             lambda a, b, c, d: pd.Series(
                 np.linspace(a, b, c), name=f"""x_{int(d)}"""
             ).round(1),
-            list(np.repeat(t_info.space_edge_min, t_info.n_features)),
-            list(np.repeat(t_info.space_edge_max - 1, t_info.n_features)),
-            list(np.repeat(t_info.space_edge_max, t_info.n_features)),
-            list(np.linspace(1, t_info.n_features, t_info.n_features)),
+            list(np.repeat(dict_info["space_edge_min"], dict_info["n_features"])),
+            list(np.repeat(dict_info["space_edge_max"] - 1, dict_info["n_features"])),
+            list(np.repeat(dict_info["space_edge_max"], dict_info["n_features"])),
+            list(np.linspace(1, dict_info["n_features"], dict_info["n_features"])),
         )
     )
     df_xy = reduce(lambda a, b: pd.merge(a, b, how="cross"), l_x)
-    if t_info.condition == "smooth":
+    if dict_info["condition"] == "smooth":
         mult = 1
-    elif t_info.condition == "rough":
+    elif dict_info["condition"] == "rough":
         mult = 3
     ivs = df_xy.columns
     df_xy["y"] = (np.sin(df_xy[ivs]) * mult).sum(axis=1)
@@ -66,22 +65,21 @@ def make_stimuli(t_info: pd.core.frame) -> pd.DataFrame:
     return df_xy
 
 
-def perceive_stimulus(df_test: pd.DataFrame, l_info: list) -> tuple:
-    """perceive a stimulus from the 2D grid using prior_sd from l_info
+def perceive_stimulus(df_test: pd.DataFrame, dict_info: dict) -> tuple:
+    """perceive a stimulus from the 2D grid using prior_sd from dict_info
 
     Args:
         df_test (pd.DataFrame): data frame with stimuli not shown during training
-        l_info (list): experimental parameter list
+        dict_info (dict): experimental parameter dict
 
     Returns:
         tuple: 1-row data frame with stimulus perceived
     """
     idx_stimulus = np.random.choice(df_test.index)
     df_stim = pd.DataFrame(df_test.loc[idx_stimulus,].copy()).T
-    df_stim["x_1"] = np.random.normal(df_stim["x_1"], l_info[0].prior_sd, 1)
-    df_stim["x_2"] = np.random.normal(df_stim["x_2"], l_info[0].prior_sd, 1)
+    df_stim["x_1"] = np.random.normal(df_stim["x_1"], dict_info["prior_sd"], 1)
+    df_stim["x_2"] = np.random.normal(df_stim["x_2"], dict_info["prior_sd"], 1)
     return (df_stim, idx_stimulus)
-    # NOTE: indexing on l_info has yet to be changed
 
 
 def fit_on_train(df_train: pd.DataFrame) -> GaussianProcessRegressor:
@@ -115,7 +113,7 @@ def predict_on_test(df: pd.DataFrame, gp: GaussianProcessRegressor) -> pd.DataFr
 
 
 def run_perception(
-    df_train: pd.DataFrame, df_test: pd.DataFrame, l_info: list
+    dict_info: dict, df_train: pd.DataFrame, df_test: pd.DataFrame
 ) -> pd.DataFrame:
     """iterate over trials noisily perceiving stimuli from test dataset and
     predicting function values using the gp model
@@ -123,15 +121,15 @@ def run_perception(
     Args:
         df_train (pd.DataFrame): data frame with stimuli shown during training
         df_test (pd.DataFrame): data frame with stimuli not shown during training
-        l_info (list): experimental parameter list
+        dict_info (dict): experimental parameter dict
 
     Returns:
         pd.DataFrame: test data frame with accepted samples appended to df_test
     """
     gp = fit_on_train(df_train)
     df_new = df_test.copy()
-    for i in tqdm(range(0, l_info[0].n_runs)):
-        df_stim, idx_stimulus = perceive_stimulus(df_test, l_info)
+    for i in tqdm(range(0, dict_info["n_runs"])):
+        df_stim, idx_stimulus = perceive_stimulus(df_test, dict_info)
         # propose a new posterior
         df_stim = predict_on_test(df_stim, gp)
         deviation_test = np.abs(
@@ -142,11 +140,11 @@ def run_perception(
         )
         df_stim["y_pred_mn"] = df_stim["y_pred_mn"]
         df_stim["y_pred_sd"] = df_stim["y_pred_sd"]
-        if l_info[0].sampling == "improvement":
+        if dict_info["sampling"] == "improvement":
             if deviation_trial < deviation_test:
                 df_new = df_new.append(df_stim, ignore_index=True)
                 # print("accepted improvement")
-        elif l_info[0].sampling == "metropolis-hastings":
+        elif dict_info["sampling"] == "metropolis-hastings":
             ecdf = ECDF(df_test["y_pred_sd"])
             prop_deviation = ecdf(deviation_trial)
             sample_uniform = np.random.uniform()
