db70efc5a356b48d5bcfa09d64a420adf3e3c63f
MirkoTh <mirkothalmann@hotmail.com>
2022-03-30 10:55:10 +0200

    add script writing bonus payments to csv

---
+++ experiments/2022-02-category-learning/R/bonus-payments.R
@@ -0,0 +1,23 @@
+library(tidyverse)
+
+file_path_paid <- "experiments/2022-02-category-learning/data/bonus-paid.csv"
+file_path_pending <- "experiments/2022-02-category-learning/data/bonus-pending.csv"
+file_path_new <- "experiments/2022-02-category-learning/data/bonus.json"
+
+tbl_bonus_paid <- read.csv(file = file_path_paid) %>% as_tibble()
+
+js_bonus <- read_file(file_path_new)
+js_bonus <-str_c("[", str_replace_all(js_bonus, "\\}", "\\},"), "]")
+js_bonus <- str_replace(js_bonus, ",\n]", "]")
+tbl_bonus_all <- jsonlite::fromJSON(js_bonus) %>% as_tibble()
+
+tbl_bonus_all <- rbind(tbl_bonus_all, c(22, 1.20, 0.5, 1.7))
+
+tbl_bonus_not_paid <- tbl_bonus_all %>% 
+  left_join(tbl_bonus_paid, by = "participant_id", suffix = c("_all", "_paid")) %>%
+  filter(is.na(bonus_total_paid)) %>%
+  select(-bonus_total_paid) %>%
+  rename(bonus_total = bonus_total_all)
+
+tbl_bonus_not_paid %>% select(participant_id, bonus_total) %>% write.table(file = file_path_paid, sep = ", ", col.names = FALSE, row.names = FALSE, append = TRUE)
+tbl_bonus_not_paid %>% select(participant_id, bonus_total) %>% write.table(file = file_path_pending, row.names = FALSE)
