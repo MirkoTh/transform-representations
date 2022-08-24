4045f411587d070a094ba9ea144f1b7483474343
Mirko Thalmann <mirkothalmann@hotmail.com>
2022-08-24 18:49:05 +0200

    add stan model for similarity ratings

--- experiments/2022-07-category-learning-II/R/bayesian-stats.R
+++ experiments/2022-07-category-learning-II/R/bayesian-stats.R
@@ -51,7 +51,6 @@ ggplot(
     y = "Nr. Correct (Out of 80)"
   )
 
-
 cat_model <- stan_cat()
 mod_cat <- cmdstan_model(cat_model)
 
@@ -98,5 +97,67 @@ tbl_thx <- l[[2]]
 map(as.list(params_bf), plot_posterior, tbl_posterior, tbl_thx, bfs)
 
 
+# Similarity Ratings ------------------------------------------------------
+
+ggplot(
+  tbl_sim %>% 
+    group_by(distance_binned) %>% 
+    summarize(response_mn = mean(response)),
+  aes(distance_binned, response_mn, group = 1)) +
+  geom_line() +
+  geom_point(size = 4, color = "white") +
+  geom_point(shape = 1) +
+  theme_bw() +
+  labs(
+    x = "Euclidean Distance (Binned)",
+    y = "Avg. Similarity Rating (1-4)"
+  )
+
+sim_model <- stan_sim()
+mod_sim <- cmdstan_model(sim_model)
+
+mm_sim <- model.matrix(
+  response ~ distance_euclidian, data = tbl_sim
+) %>% as_tibble()
+colnames(mm_sim) <- c("ic", "d_euclidean")
+mm_sim$d_euclidean <- scale(mm_sim$d_euclidean)[, 1]
 
 
+mod_sim$variables()$data
+
+l_data <- list(
+  n_data = nrow(tbl_sim),
+  n_subj = length(unique(tbl_sim$participant_id)),
+  response = tbl_sim$response,
+  subj = as.numeric(factor(
+    tbl_sim$participant_id, 
+    labels = 1:length(unique(tbl_sim$participant_id))
+  )),
+  x = as.matrix(mm_sim)
+)
+
+fit_sim <- mod_sim$sample(
+  data = l_data, iter_sampling = 1000, iter_warmup = 1000, chains = 1
+)
+
+file_loc <- str_c("experiments/2022-07-category-learning-II/data/sim-model.RDS")
+fit_sim$save_object(file = file_loc)
+pars_interest <- c("mu_tf")
+tbl_draws <- fit_sim$draws(variables = pars_interest, format = "df")
+tbl_summary <- fit_sim$summary(variables = pars_interest)
+
+
+params_bf <- c("Intercept", "Euclidean Distance")
+
+tbl_posterior <- tbl_draws %>% 
+  dplyr::select(starts_with(c("mu")), .chain) %>%
+  rename(chain = .chain) %>%
+  pivot_longer(starts_with(c("mu")), names_to = "parameter", values_to = "value") %>%
+  mutate(parameter = factor(parameter, labels = params_bf))
+
+l <- sd_bfs(tbl_posterior, params_bf, sqrt(2)/4)
+bfs <- l[[1]]
+tbl_thx <- l[[2]]
+
+# plot the posteriors and the bfs
+map(as.list(params_bf), plot_posterior, tbl_posterior, tbl_thx, bfs)
--- experiments/2022-07-category-learning-II/R/stan-models.R
+++ experiments/2022-07-category-learning-II/R/stan-models.R
@@ -51,3 +51,59 @@ model {
 ")
   return(stan_logistic)
 }
+
+
+stan_sim <- function() {
+  
+  stan_normal_sim <- write_stan_file("
+data {
+  int n_data;
+  int n_subj;
+  vector[n_data] response;
+  array[n_data] int subj;
+  matrix[n_data, 2] x; // ic, trial
+}
+
+transformed data {
+  real scale_cont = sqrt(2) / 4;
+  real scale_cat = 1.0/2;
+}
+
+parameters {
+  matrix[n_subj, 2] b;
+  vector[2] mu;
+  vector <lower=0>[2] sigma_subject;
+  real<lower=0> sigma;
+}
+
+transformed parameters {
+  array[2] real mu_tf;
+  mu_tf[1] = mu[1];
+  mu_tf[2] = scale_cont * mu[2];
+  vector[n_data] mn;
+
+  for (n in 1:n_data) {
+    mn[n] = b[subj[n], 1] * x[n, 1] + b[subj[n], 2] * x[n, 2];
+  }
+}
+
+model {
+  for (n in 1:n_data) {
+    response[n] ~ normal(mn[n], sigma);
+  }
+
+  for (s in 1:n_subj) {
+    b[s, 1] ~ normal(mu_tf[1], sigma_subject[1]);
+    b[s, 2] ~ normal(mu_tf[2], sigma_subject[2]);
+  }
+  
+  sigma ~ uniform(0.001, 10);
+  sigma_subject[1] ~ uniform(0.001, 100);
+  sigma_subject[2] ~ uniform(0.001, 100);
+  mu[1] ~ normal(0, 1);
+  mu[2] ~ student_t(1, 0, 1);
+}
+
+")
+  return(stan_normal_sim)
+}
