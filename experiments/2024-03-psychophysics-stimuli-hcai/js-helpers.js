0895175111a41e2ea50676197a23efa91adcf134
MirkoTh <mirkothalmann@hotmail.com>
2024-03-08 18:47:25 +0100

    first draft triplet psychophysics study

---
+++ experiments/2024-03-psychophysics-stimuli-hcai/js-helpers.js
@@ -0,0 +1,234 @@
+var stimulus_set_1 = [
+  [0,2,3], [0,2,4], [0,2,6], [0,2,7], [0,2,8], [0,2,9], [0,2,10], [0,2,11], [0,2,12], [0,2,13], [0,3,4], [0,3,6], [0,3,7], [0,3,9], [0,3,10], [0,3,11], [0,3,12], [0,3,13], [0,4,6], [0,4,7], [0,4,11], [0,5,7], [0,5,8], [0,5,11], [0,6,7], [0,6,8], [0,6,10], [0,6,11], [0,6,12], [0,7,11], [0,8,9], [0,8,10], [0,8,11], [0,8,12], [0,9,12], [0,10,12], [0,11,13], [0,12,13], [1,2,4], [1,2,13], [1,3,6], [1,3,7], [1,3,13], [1,4,5], [1,4,6], [1,4,7], [1,4,11], [1,4,12], [1,5,6], [1,5,7], [1,5,9], [1,5,10], [1,5,13], [1,6,8], [1,6,10], [1,6,13], [1,7,8], [1,7,9], [1,7,11], [1,7,12], [1,7,13], [1,8,12], [1,8,13], [1,9,10], [1,9,12], [1,10,12], [1,11,13], [1,12,13], [2,3,4], [2,3,7], [2,3,8], [2,3,9], [2,3,10], [2,3,11], [2,3,13], [2,4,5], [2,4,8], [2,4,9], [2,4,12], [2,5,6], [2,5,9], [2,5,11], [2,6,9], [2,6,10], [2,6,12], [2,6,13], [2,7,9], [2,7,13], [2,8,9], [2,8,10], [2,8,13], [2,9,10], [2,9,12], [2,9,13], [2,10,12], [2,11,13], [2,12,13], [3,4,5], [3,4,6], [3,4,9], [3,4,10], [3,4,12], [3,4,13], [3,5,7], [3,5,8], [3,5,9], [3,5,11], [3,5,12], [3,5,13], [3,6,8], [3,6,11], [3,6,13], [3,7,9], [3,7,11], [3,7,12], [3,8,10], [3,8,11], [3,8,13], [3,9,10], [3,9,13], [3,10,12], [3,11,13], [3,12,13], [4,5,9], [4,6,7], [4,6,8], [4,6,11], [4,7,8], [4,7,11], [4,7,13], [4,8,11], [4,9,10], [4,9,11], [4,10,12], [4,11,13], [4,12,13], [5,6,8], [5,6,9], [5,6,11], [5,6,12], [5,6,13], [5,7,9], [5,7,10], [5,7,11], [5,7,13], [5,8,9], [5,8,10], [5,8,11], [5,9,11], [5,9,12], [5,10,12], [5,10,13], [5,11,13], [5,12,13], [6,7,11], [6,8,9], [6,8,12], [6,8,13], [6,9,12], [6,9,13], [6,10,13], [6,11,13], [6,12,13], [7,8,9], [7,8,10], [7,8,13], [7,9,12], [7,9,13], [7,10,11], [7,11,13], [7,12,13], [8,9,10], [8,9,13], [8,10,11], [8,10,12], [8,12,13], [9,10,12], [9,11,12], [9,11,13], [9,12,13], [10,12,13], [11,12,13]
+  ];
+
+var stimulus_set_2 = [
+  [0,1,2], [0,1,3], [0,1,4], [0,1,5], [0,1,6], [0,1,7], [0,1,8], [0,1,9], [0,1,10], [0,1,11], [0,1,12], [0,1,13], [0,2,5], [0,3,5], [0,3,8], [0,4,5], [0,4,8], [0,4,9], [0,4,10], [0,4,12], [0,4,13], [0,5,6], [0,5,9], [0,5,10], [0,5,12], [0,5,13], [0,6,9], [0,6,13], [0,7,8], [0,7,9], [0,7,10], [0,7,12], [0,7,13], [0,8,13], [0,9,10], [0,9,11], [0,9,13], [0,10,11], [0,10,13], [0,11,12], [1,2,3], [1,2,5], [1,2,6], [1,2,7], [1,2,8], [1,2,9], [1,2,10], [1,2,11], [1,2,12], [1,3,4], [1,3,5], [1,3,8], [1,3,9], [1,3,10], [1,3,11], [1,3,12], [1,4,8], [1,4,9], [1,4,10], [1,4,13], [1,5,8], [1,5,11], [1,5,12], [1,6,7], [1,6,9], [1,6,11], [1,6,12], [1,7,10], [1,8,9], [1,8,10], [1,8,11], [1,9,11], [1,9,13], [1,10,11], [1,10,13], [1,11,12], [2,3,5], [2,3,6], [2,3,12], [2,4,6], [2,4,7], [2,4,10], [2,4,11], [2,4,13], [2,5,7], [2,5,8], [2,5,10], [2,5,12], [2,5,13], [2,6,7], [2,6,8], [2,6,11], [2,7,8], [2,7,10], [2,7,11], [2,7,12], [2,8,11], [2,8,12], [2,9,11], [2,10,11], [2,10,13], [2,11,12], [3,4,7], [3,4,8], [3,4,11], [3,5,6], [3,5,10], [3,6,7], [3,6,9], [3,6,10], [3,6,12], [3,7,8], [3,7,10], [3,7,13], [3,8,9], [3,8,12], [3,9,11], [3,9,12], [3,10,11], [3,10,13], [3,11,12], [4,5,6], [4,5,7], [4,5,8], [4,5,10], [4,5,11], [4,5,12], [4,5,13], [4,6,9], [4,6,10], [4,6,12], [4,6,13], [4,7,9], [4,7,10], [4,7,12], [4,8,9], [4,8,10], [4,8,12], [4,8,13], [4,9,12], [4,9,13], [4,10,11], [4,10,13], [4,11,12], [5,6,7], [5,6,10], [5,7,8], [5,7,12], [5,8,12], [5,8,13], [5,9,10], [5,9,13], [5,10,11], [5,11,12], [6,7,8], [6,7,9], [6,7,10], [6,7,12], [6,7,13], [6,8,10], [6,8,11], [6,9,10], [6,9,11], [6,10,11], [6,10,12], [6,11,12], [7,8,11], [7,8,12], [7,9,10], [7,9,11], [7,10,12], [7,10,13], [7,11,12], [8,9,11], [8,9,12], [8,10,13], [8,11,12], [8,11,13], [9,10,11], [9,10,13], [10,11,12], [10,11,13]  
+];
+
+
+
+const n_trials_per_dim = stimulus_set_1.length;
+const session = 1;
+
+if (session == 1) {
+  var stimulus_triplets = stimulus_set_1;
+}
+
+var tmp = Array(stimulus_set_1.length)
+  .fill()
+  .map((element, index) => index);
+var stimulus_ids_head = append_randomized_arrays(tmp, 1);
+var stimulus_ids_belly = append_randomized_arrays(tmp, 1);
+
+// 1: 123
+// 2: 321
+
+var stimulus_orderings = [1, 2];
+const n_reps_orderings = n_trials_per_dim / stimulus_orderings.length;
+var stimulus_orderings_head = append_randomized_arrays(
+  stimulus_orderings,
+  n_reps_orderings
+);
+// randomize again to make order irrelevant
+stimulus_orderings_head = append_randomized_arrays(stimulus_orderings_head, 1);
+var stimulus_orderings_belly = append_randomized_arrays(
+  stimulus_orderings,
+  n_reps_orderings
+);
+// randomize again to make order irrelevant
+stimulus_orderings_belly = append_randomized_arrays(
+  stimulus_orderings_belly,
+  1
+);
+
+var stimulus_locations = [];
+
+const images_head = [
+  "./stimuli/stimulus[11,50].png",
+  "./stimuli/stimulus[17,50].png",
+  "./stimuli/stimulus[23,50].png",
+  "./stimuli/stimulus[29,50].png",
+  "./stimuli/stimulus[35,50].png",
+  "./stimuli/stimulus[41,50].png",
+  "./stimuli/stimulus[47,50].png",
+  "./stimuli/stimulus[53,50].png",
+  "./stimuli/stimulus[59,50].png",
+  "./stimuli/stimulus[65,50].png",
+  "./stimuli/stimulus[71,50].png",
+  "./stimuli/stimulus[77,50].png",
+  "./stimuli/stimulus[83,50].png",
+  "./stimuli/stimulus[89,50].png",
+];
+
+const images_belly = [
+  "./stimuli/stimulus[50,11].png",
+  "./stimuli/stimulus[50,17].png",
+  "./stimuli/stimulus[50,23].png",
+  "./stimuli/stimulus[50,29].png",
+  "./stimuli/stimulus[50,35].png",
+  "./stimuli/stimulus[50,41].png",
+  "./stimuli/stimulus[50,47].png",
+  "./stimuli/stimulus[50,53].png",
+  "./stimuli/stimulus[50,59].png",
+  "./stimuli/stimulus[50,65].png",
+  "./stimuli/stimulus[50,71].png",
+  "./stimuli/stimulus[50,77].png",
+  "./stimuli/stimulus[50,83].png",
+  "./stimuli/stimulus[50,89].png",
+];
+
+var timeline_variables_head = [];
+var tmp_ordering = [];
+var current_stimulus_ids = [];
+for (var i = 0; i < n_trials_per_dim; i++) {
+  switch (stimulus_orderings_head[i]) {
+    case 1:
+      tmp_ordering = [0, 1, 2];
+      break;
+    case 2:
+      tmp_ordering = [2, 1, 0];
+      break;
+  }
+
+
+  var current_stimulus_ids = stimulus_triplets[stimulus_ids_head[i]];
+  var id_left_probe = current_stimulus_ids[tmp_ordering[0]];
+  var id_right_probe = current_stimulus_ids[tmp_ordering[2]];
+  var id_target = current_stimulus_ids[tmp_ordering[1]];
+
+  var trial_details = {
+    trial_id: i,
+    upleftImage: images_head[id_left_probe],
+    uprightImage: images_head[id_right_probe],
+    lomidImage: images_head[id_target],
+    id_left_probe: id_left_probe,
+    id_right_probe: id_right_probe,
+    id_target: id_target,
+    stimulus_ids: current_stimulus_ids,
+    stimulus_ordering: tmp_ordering,
+  };
+
+  timeline_variables_head.push(trial_details);
+}
+
+var is_practice = 0;
+
+// Preloading files are needed to present the stimuli accurately.
+const preload = {
+  type: jsPsychPreload,
+  images: images_head,
+};
+
+const instruction = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: "Click on the Start button.",
+  choices: ["Start"],
+  prompt: "This is a psychophysics study.",
+};
+
+function append_randomized_arrays(set, n) {
+  var sets_randomized = [];
+  for (let i = 0; i < n; i++) {
+    var set_permuted = permute(set);
+    sets_randomized = sets_randomized.concat(set_permuted);
+  }
+  return sets_randomized;
+}
+
+//permute a list
+function permute(o) {
+  for (
+    var j, x, i = o.length;
+    i;
+    j = Math.floor(Math.random() * i), x = o[--i], o[i] = o[j], o[j] = x
+  );
+  return o;
+}
+
+// get subject ID
+function getQueryVariable(variable) {
+  var query = window.location.search.substring(1);
+  var vars = query.split("&");
+  for (var i = 0; i < vars.length; i++) {
+    var pair = vars[i].split("=");
+    if (pair[0] == variable) {
+      return pair[1];
+    }
+  }
+  return false;
+}
+
+function download(content, fileName, contentType) {
+  var a = document.createElement("a");
+  var file = new Blob([content], { type: contentType });
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  a.click();
+}
+
+function saveData(filedata, filename, task, flag_save = true) {
+  //var filename = "./data/" + task + "-participant-" + participant_id + ".json";
+  var filename_folder = "../.././data/" + task + "/" + filename;
+  $.post("save_data.php", {
+    postresult: filedata + "\n",
+    postfile: filename_folder,
+  });
+  if (flag_save) {
+    dataSaved = true;
+  }
+}
+
+function checkDataSaving() {
+  if (!dataSaved) {
+    // Data saving is not complete, wait for a few seconds and check again.
+    setTimeout(checkDataSaving, 2000); // Wait for 2 seconds and check again.
+  } else {
+    // Data saving is complete, you can proceed with the next steps.
+    console.log("Data has been saved successfully.");
+  }
+}
+
+async function saveSeveralData(filedata, filename, task) {
+  //var filename = "./data/" + task + "-participant-" + participant_id + ".json";
+  var filename_folder = "../.././data/" + task + "/" + filename;
+  var n_data = filedata.length;
+  for (var i = 0; i < n_data; i++) {
+    $.post("save_data.php", {
+      postresult: JSON.stringify(filedata[i]) + "\n",
+      postfile: filename_folder,
+    });
+  }
+  dataSaved = true;
+}
+
+async function saveSeveralDataOverwrite(filedata, filename, task) {
+  //var filename = "./data/" + task + "-participant-" + participant_id + ".json";
+  var filename_folder = "../.././data/" + task + "/" + filename;
+  var n_data = filedata.length;
+  for (var i = 0; i < n_data; i++) {
+    if (i == 0) {
+      $.post("save_data_overwrite.php", {
+        postresult: JSON.stringify(filedata[i]) + "\n",
+        postfile: filename_folder,
+      });
+    } else if (i > 0) {
+      $.post("save_data.php", {
+        postresult: JSON.stringify(filedata[i]) + "\n",
+        postfile: filename_folder,
+      });
+    }
+  }
+  dataSaved = true;
+}
+
+function updateQueryStringParameter(uri, key, value) {
+  var re = new RegExp("([?&])" + key + "=.*?(&|$)", "i");
+  var separator = uri.indexOf("?") !== -1 ? "&" : "?";
+  if (uri.match(re)) {
+    return uri.replace(re, "$1" + key + "=" + value + "$2");
+  } else {
+    return uri + separator + key + "=" + value;
+  }
+}
+
