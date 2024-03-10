8826c0860e271ad5e22a9d6b9783af2c54c943a2
MirkoTh <mirkothalmann@hotmail.com>
2024-03-10 20:05:00 +0100

    first version of similarity study

--- experiments/2024-03-psychophysics-stimuli-hcai/js-helpers.js
+++ experiments/2024-03-psychophysics-stimuli-hcai/js-helpers.js
@@ -6,13 +6,23 @@ var stimulus_set_2 = [
   [0,1,2], [0,1,3], [0,1,4], [0,1,5], [0,1,6], [0,1,7], [0,1,8], [0,1,9], [0,1,10], [0,1,11], [0,1,12], [0,1,13], [0,2,5], [0,3,5], [0,3,8], [0,4,5], [0,4,8], [0,4,9], [0,4,10], [0,4,12], [0,4,13], [0,5,6], [0,5,9], [0,5,10], [0,5,12], [0,5,13], [0,6,9], [0,6,13], [0,7,8], [0,7,9], [0,7,10], [0,7,12], [0,7,13], [0,8,13], [0,9,10], [0,9,11], [0,9,13], [0,10,11], [0,10,13], [0,11,12], [1,2,3], [1,2,5], [1,2,6], [1,2,7], [1,2,8], [1,2,9], [1,2,10], [1,2,11], [1,2,12], [1,3,4], [1,3,5], [1,3,8], [1,3,9], [1,3,10], [1,3,11], [1,3,12], [1,4,8], [1,4,9], [1,4,10], [1,4,13], [1,5,8], [1,5,11], [1,5,12], [1,6,7], [1,6,9], [1,6,11], [1,6,12], [1,7,10], [1,8,9], [1,8,10], [1,8,11], [1,9,11], [1,9,13], [1,10,11], [1,10,13], [1,11,12], [2,3,5], [2,3,6], [2,3,12], [2,4,6], [2,4,7], [2,4,10], [2,4,11], [2,4,13], [2,5,7], [2,5,8], [2,5,10], [2,5,12], [2,5,13], [2,6,7], [2,6,8], [2,6,11], [2,7,8], [2,7,10], [2,7,11], [2,7,12], [2,8,11], [2,8,12], [2,9,11], [2,10,11], [2,10,13], [2,11,12], [3,4,7], [3,4,8], [3,4,11], [3,5,6], [3,5,10], [3,6,7], [3,6,9], [3,6,10], [3,6,12], [3,7,8], [3,7,10], [3,7,13], [3,8,9], [3,8,12], [3,9,11], [3,9,12], [3,10,11], [3,10,13], [3,11,12], [4,5,6], [4,5,7], [4,5,8], [4,5,10], [4,5,11], [4,5,12], [4,5,13], [4,6,9], [4,6,10], [4,6,12], [4,6,13], [4,7,9], [4,7,10], [4,7,12], [4,8,9], [4,8,10], [4,8,12], [4,8,13], [4,9,12], [4,9,13], [4,10,11], [4,10,13], [4,11,12], [5,6,7], [5,6,10], [5,7,8], [5,7,12], [5,8,12], [5,8,13], [5,9,10], [5,9,13], [5,10,11], [5,11,12], [6,7,8], [6,7,9], [6,7,10], [6,7,12], [6,7,13], [6,8,10], [6,8,11], [6,9,10], [6,9,11], [6,10,11], [6,10,12], [6,11,12], [7,8,11], [7,8,12], [7,9,10], [7,9,11], [7,10,12], [7,10,13], [7,11,12], [8,9,11], [8,9,12], [8,10,13], [8,11,12], [8,11,13], [9,10,11], [9,10,13], [10,11,12], [10,11,13]  
 ];
 
+// use a fixed set of practice stimuli
+// same for both dimemsions
+var sample_ids = [88, 32, 9, 1, 179];
+var stimulus_set_practice = sample_ids.map(x=>stimulus_set_1[x]);
+console.log("stimulus_set_practice: ", stimulus_set_practice);  
 
 
 const n_trials_per_dim = stimulus_set_1.length;
+const n_trials_practice = sample_ids.length;
 const session = 1;
 
 if (session == 1) {
-  var stimulus_triplets = stimulus_set_1;
+  var stimulus_triplets_head = stimulus_set_1;
+  var stimulus_triplets_belly = stimulus_set_2;
+} else if (session == 2) {
+  var stimulus_triplets_head = stimulus_set_2;
+  var stimulus_triplets_belly = stimulus_set_1;
 }
 
 var tmp = Array(stimulus_set_1.length)
@@ -20,9 +30,9 @@ var tmp = Array(stimulus_set_1.length)
   .map((element, index) => index);
 var stimulus_ids_head = append_randomized_arrays(tmp, 1);
 var stimulus_ids_belly = append_randomized_arrays(tmp, 1);
-
-// 1: 123
-// 2: 321
+var stimulus_triplets_head_practice = stimulus_set_practice;
+var stimulus_triplets_belly_practice = stimulus_set_practice;
+var stimulus_orderings_practice = [1, 2, 1, 2, 1];
 
 var stimulus_orderings = [1, 2];
 const n_reps_orderings = n_trials_per_dim / stimulus_orderings.length;
@@ -78,9 +88,80 @@ const images_belly = [
   "./stimuli/stimulus[50,89].png",
 ];
 
+
 var timeline_variables_head = [];
+var timeline_variables_belly = [];
+var timeline_variables_belly_practice = [];
+var timeline_variables_head_practice = [];
 var tmp_ordering = [];
 var current_stimulus_ids = [];
+
+
+for (var i = 0; i < n_trials_practice; i++) {
+  switch (stimulus_orderings_practice[i]) {
+    case 1:
+      tmp_ordering = [0, 1, 2];
+      break;
+    case 2:
+      tmp_ordering = [2, 1, 0];
+      break;
+  }
+
+  var current_stimulus_ids = stimulus_triplets_belly_practice[i];
+  var id_left_probe = current_stimulus_ids[tmp_ordering[0]];
+  var id_right_probe = current_stimulus_ids[tmp_ordering[2]];
+  var id_target = current_stimulus_ids[tmp_ordering[1]];
+
+  var trial_details = {
+    is_practice: 1,
+    trial_id: i,
+    dimension: "belly",
+    upleftImage: images_belly[id_left_probe],
+    uprightImage: images_belly[id_right_probe],
+    lomidImage: images_belly[id_target],
+    id_left_probe: id_left_probe,
+    id_right_probe: id_right_probe,
+    id_target: id_target,
+    stimulus_ids: current_stimulus_ids,
+    stimulus_ordering: tmp_ordering,
+  };
+
+  timeline_variables_belly_practice.push(trial_details);
+}
+
+
+for (var i = 0; i < n_trials_practice; i++) {
+  switch (stimulus_orderings_practice[i]) {
+    case 1:
+      tmp_ordering = [0, 1, 2];
+      break;
+    case 2:
+      tmp_ordering = [2, 1, 0];
+      break;
+  }
+
+  var current_stimulus_ids = stimulus_triplets_head_practice[i];
+  var id_left_probe = current_stimulus_ids[tmp_ordering[0]];
+  var id_right_probe = current_stimulus_ids[tmp_ordering[2]];
+  var id_target = current_stimulus_ids[tmp_ordering[1]];
+
+  var trial_details = {
+    is_practice: 1,
+    trial_id: i,
+    dimension: "head",
+    upleftImage: images_head[id_left_probe],
+    uprightImage: images_head[id_right_probe],
+    lomidImage: images_head[id_target],
+    id_left_probe: id_left_probe,
+    id_right_probe: id_right_probe,
+    id_target: id_target,
+    stimulus_ids: current_stimulus_ids,
+    stimulus_ordering: tmp_ordering,
+  };
+
+  timeline_variables_head_practice.push(trial_details);
+}
+
 for (var i = 0; i < n_trials_per_dim; i++) {
   switch (stimulus_orderings_head[i]) {
     case 1:
@@ -91,14 +172,15 @@ for (var i = 0; i < n_trials_per_dim; i++) {
       break;
   }
 
-
-  var current_stimulus_ids = stimulus_triplets[stimulus_ids_head[i]];
+  var current_stimulus_ids = stimulus_triplets_head[stimulus_ids_head[i]];
   var id_left_probe = current_stimulus_ids[tmp_ordering[0]];
   var id_right_probe = current_stimulus_ids[tmp_ordering[2]];
   var id_target = current_stimulus_ids[tmp_ordering[1]];
 
   var trial_details = {
+    is_practice: 0,
     trial_id: i,
+    dimension: "head",
     upleftImage: images_head[id_left_probe],
     uprightImage: images_head[id_right_probe],
     lomidImage: images_head[id_target],
@@ -112,19 +194,42 @@ for (var i = 0; i < n_trials_per_dim; i++) {
   timeline_variables_head.push(trial_details);
 }
 
-var is_practice = 0;
+for (var i = 0; i < n_trials_per_dim; i++) {
+  switch (stimulus_orderings_belly[i]) {
+    case 1:
+      tmp_ordering = [0, 1, 2];
+      break;
+    case 2:
+      tmp_ordering = [2, 1, 0];
+      break;
+  }
+
+  var current_stimulus_ids = stimulus_triplets_belly[stimulus_ids_belly[i]];
+  var id_left_probe = current_stimulus_ids[tmp_ordering[0]];
+  var id_right_probe = current_stimulus_ids[tmp_ordering[2]];
+  var id_target = current_stimulus_ids[tmp_ordering[1]];
+
+  var trial_details = {
+    is_practice: 0,
+    trial_id: i,
+    dimension: "belly",
+    upleftImage: images_belly[id_left_probe],
+    uprightImage: images_belly[id_right_probe],
+    lomidImage: images_belly[id_target],
+    id_left_probe: id_left_probe,
+    id_right_probe: id_right_probe,
+    id_target: id_target,
+    stimulus_ids: current_stimulus_ids,
+    stimulus_ordering: tmp_ordering,
+  };
+
+  timeline_variables_belly.push(trial_details);
+}
 
 // Preloading files are needed to present the stimuli accurately.
 const preload = {
   type: jsPsychPreload,
-  images: images_head,
-};
-
-const instruction = {
-  type: jsPsychHtmlButtonResponse,
-  stimulus: "Click on the Start button.",
-  choices: ["Start"],
-  prompt: "This is a psychophysics study.",
+  images: [images_head, images_belly],
 };
 
 function append_randomized_arrays(set, n) {
@@ -167,9 +272,9 @@ function download(content, fileName, contentType) {
   a.click();
 }
 
-function saveData(filedata, filename, task, flag_save = true) {
+function saveData(filedata, filename, flag_save = true) {
   //var filename = "./data/" + task + "-participant-" + participant_id + ".json";
-  var filename_folder = "../.././data/" + task + "/" + filename;
+  var filename_folder = "../.././data/" + filename;
   $.post("save_data.php", {
     postresult: filedata + "\n",
     postfile: filename_folder,
@@ -189,9 +294,9 @@ function checkDataSaving() {
   }
 }
 
-async function saveSeveralData(filedata, filename, task) {
+async function saveSeveralData(filedata, filename) {
   //var filename = "./data/" + task + "-participant-" + participant_id + ".json";
-  var filename_folder = "../.././data/" + task + "/" + filename;
+  var filename_folder = "../.././data/" + filename;
   var n_data = filedata.length;
   for (var i = 0; i < n_data; i++) {
     $.post("save_data.php", {
@@ -202,9 +307,8 @@ async function saveSeveralData(filedata, filename, task) {
   dataSaved = true;
 }
 
-async function saveSeveralDataOverwrite(filedata, filename, task) {
-  //var filename = "./data/" + task + "-participant-" + participant_id + ".json";
-  var filename_folder = "../.././data/" + task + "/" + filename;
+async function saveSeveralDataOverwrite(filedata, filename) {
+  var filename_folder = "../.././data/" + filename;
   var n_data = filedata.length;
   for (var i = 0; i < n_data; i++) {
     if (i == 0) {
@@ -231,4 +335,125 @@ function updateQueryStringParameter(uri, key, value) {
     return uri + separator + key + "=" + value;
   }
 }
+
+
+var comprehension_question_allinone = {
+  type: jsPsychSurveyMultiChoice,
+  questions: [
+      {
+          prompt: '<div align=center><b>What is your goal in this study?</b></div>',
+          options: [
+              'Compare the two objects with each other.',
+              'Compare the upper two objects with each other.',
+              'Mark the most dissimilar item of the three as the odd-one-out.',
+              'Say which of the two objects presented above is most similar to the one below.'
+          ],
+
+          required: true
+      }, {
+          prompt: '<div align=center><b>How do you respond in the task?</b></div>',
+          options: [
+              'left arrow: bottom object more similar to top left object. right arrow: bottom object more similar to top right object.',
+              'Left/right arrow: top object more similar to bottom left/right object.',
+              'This is a passive viewing task. I do not have to resond in any way.',
+              'f: bottom object more similar to top left object. j: bottom object more similar to top right object.'
+          ],
+
+          required: true
+      }, {
+          prompt: '<div align=center><b>How long does the study take approximately?</b></div>',
+          options: [
+              'It is a short one, approx. 5 mins.',
+              'About 15-20 mins.',
+              'It is relatively long, approx. 2 hours',
+              'I cannot remember.'
+          ],
+
+          required: true
+      }, {
+          prompt: '<div align=center><b>What purpose served the practice trials?</b></div>',
+          options: [
+              'I can maximize my reward by performing well on them.',
+              'I can leave them out if I want to.',
+              'They are used to determine the final reward in the study.',
+              'I should use them to get used to the task procedure.'
+          ],
+
+          required: true
+      },
+  ],
+  preamble: "<h3>Please answer the following questions.</h3>",
+  randomize_question_order: true,
+
+  on_finish: function (data) {
+      //var data = jsPsych.data.getLastTrialData().values()[0]; 
+      var answer_Q1 = data.response.Q0;
+      var answer_Q2 = data.response.Q1;
+      console.log("data.response.Q0 = ", data.response.Q0);
+      console.log("data.response.Q1 = ", data.response.Q1);
+      console.log("data.response.Q2 = ", data.response.Q2);
+      console.log("data.response.Q3 = ", data.response.Q3);
+      var answer_Q3 = data.response.Q2;
+      var answer_Q4 = data.response.Q3;
+
+      if (
+          answer_Q1 == 'Say which of the two objects presented above is most similar to the one below.' &&
+          answer_Q2 == 'f: bottom object more similar to top left object. j: bottom object more similar to top right object.' &&
+          answer_Q3 == 'About 15-20 mins.' &&
+          answer_Q4 == 'I should use them to get used to the task procedure.'
+
+      ) {
+          data.correct = true
+      } else {
+          data.correct = false
+      }
+  }
+};
+
+
+var comp_feedback_verbose = {
+  type: jsPsychHtmlButtonResponse,
+  stimulus: function () {
+      var last_resp_correct = jsPsych.data.get().last(1).filter({ correct: true });
+      if (last_resp_correct.count() == 1) {
+          var info = "<p align='center'><b>Well done! You answered all questions correctly.<br><br></b></p>"
+      } else {
+          var info = "<p align='center'><b>Not all questions were answered correctly.<br>Below you get feedback, which questions you got wrong.<br>Correct responses are printed in <span style='color: green'>green</span>, incorrect responses in <span style='color: red'>red</span>.<br><br></b> Please try again. <br><br></p>"
+      }
+
+      var q_responses = jsPsych.data.get().last(1).values();
+      var answer_Q1 = q_responses[0].response.Q0;
+      var answer_Q2 = q_responses[0].response.Q1;
+      var answer_Q3 = q_responses[0].response.Q2;
+      var answer_Q4 = q_responses[0].response.Q3;
+      var qna1 = "<b>What is your goal in this study?<br>Your response: </b>" + answer_Q1 + "<br>";
+      var qna2 = "<b>How do you respond in the task?<br>Your response: </b>" + answer_Q2 + "<br>";
+      var qna3 = "<b>How long does the study take approximately?<br>Your response: </b>" + answer_Q3 + "<br>";
+      var qna4 = "<b>What purpose served the practice trials?<br>Your response: </b>" + answer_Q4 + "<br>";
+
+      var explain1 = "<b>Hint:</b> Tell us which of the two upper objects is more similar to the bottom object.<br><br></p>";
+      var explain2 = "<b>Hint:</b> 'f' is on the left side of the keyboard, 'j' is on the right side of the keyboard.<br><br></p>";
+      var explain3 = "<b>Hint:</b> Not terribly long, but a bit more than 5 mins!<br><br></p>";
+      var explain4 = "<b>Hint:</b> They were just to help you getting used to the task.<br><br></p>";
+
+      if (answer_Q1 == 'Say which of the two objects presented above is most similar to the one below.') {
+          var t1 = '<p style="color:green;align=center">' + "<b>CORRECT! </b>" + qna1
+      } else { var t1 = '<p style="color:red;align=center"">' + "<b>INCORRECT! </b>" + qna1 + explain1 }
+      if (answer_Q2 == 'f: bottom object more similar to top left object. j: bottom object more similar to top right object.') {
+          var t2 = '<p style="color:green;align=center"">' + "<b>CORRECT! </b>" + qna2
+      } else { var t2 = '<p style="color:red;align=center"">' + "<b>INCORRECT! </b>" + qna2 + explain2 }
+      if (answer_Q3 == 'About 15-20 mins.') {
+          var t3 = '<p style="color:green;align=center"">' + "<b>CORRECT! </b>" + qna3
+      } else { var t3 = '<p style="color:red;align=center"">' + "<b>INCORRECT! </b>" + qna3 + explain3 }
+      if (answer_Q4 == 'I should use them to get used to the task procedure.') {
+          var t4 = '<p style="color:green;align=center"">' + "<b>CORRECT! </b>" + qna4
+      } else { var t4 = '<p style="color:red;align=center"">' + "<b>INCORRECT! </b>" + qna4 + explain4 }
+
+      var pg = info + t1 + t2 + t3 + t4;
+
+      return pg
+  },
+  choices: ['Next'],
+};
+
 
